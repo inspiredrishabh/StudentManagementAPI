3ddbfb6a57af91786509d7de9b3d7708e975d26b
inspiredrishabh <160398029+inspiredrishabh@users.noreply.github.com>
2025-04-25 18:02:21 +0530

    express Initialised

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+mongoose
+  .connect("URL", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-
-app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
